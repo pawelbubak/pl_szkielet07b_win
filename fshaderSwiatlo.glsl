514a2e896f5edd0a0f3dd72bd6ff7b0d709a98cd
Stapior <sensaj10@gmail.com>
2018-06-28 13:48:46 +0200

    ok

--- fshaderSwiatlo.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
-
-void main(void) {
-
-
-	pixelColor= vec4(1,0.95,0.8,1);
-}
